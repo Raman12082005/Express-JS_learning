711baecf88f67326c9e1f4d606387efa068ba05f
Raman12082005 <rdeep1208y@gmail.com>
2026-08-27 21:36:01 +0530

    add

---
+++ 03_NPM_Basics_ans_Advanced_Fearures/script.js
@@ -0,0 +1,18 @@
+// NPM Understanding 
+// npm - node package manager
+
+// installing and uninstalling anything basics & advanced
+// install -> npm i packageKaNaam
+
+// uninstall -> nom uninstall packageKaNaam
+// understanding node_modules
+// how to install a particular version -> npm i packageKaNaam{@version}
+
+// understanding node_modules
+
+
+// depandencies -> packages and packages ki dependencies
+// devdependencies -> aise packages jo sirf development mein kaam aayenge par jab aap ban 
+//                    jayega aur deploy hob jayega tab hum use nahi kr rhe honge
+
+
